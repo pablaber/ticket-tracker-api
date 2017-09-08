ee82f2836cd5e4ffbf0732def5a2ec0be560806c
Patrick Bacon-Blaber <pablaber225@gmail.com>
2017-09-08 17:12:08 -0400

    filter initial results

--- scrape.js
+++ scrape.js
@@ -1,1 +1,50 @@
-console.log("hello world");
+var request = require('request');
+var cheerio = require('cheerio');
+var moment = require('moment');
+
+scrape();
+
+function scrape() {
+    if(!process.argv[3]) {
+        console.log("usage: node scrape <url> \"<venue>\"");
+    }
+    else {
+        var page = process.argv[2];
+        var venue = process.argv[3];
+        var SEAT_GEEK = "https://seatgeek.com/";
+        getTodaysPrices(SEAT_GEEK + page, venue);
+    }
+}
+
+function getTodaysPrices(forUrl, forVenue) {
+    request(forUrl, function(error, resopnse, html) {
+        if(!error) {
+            var $ = cheerio.load(html);
+
+            $('.page-event-listing').filter(function() {
+                var data = $(this);
+                var title = data.children('.event-listing-details').first()
+                                .children('.event-listing-title').first()
+                                .children().first().text().trim();
+
+                var location = data.children('.event-listing-details').first()
+                                   .children('.event-listing-location').first()
+                                   .children('.event-listing-location-text').first()
+                                   .children('.event-listing-venue-link').first()
+                                   .children().first().text().trim();
+
+                var preseason = title.toLowerCase().indexOf('preseason') > -1;
+
+                // TODO pass as parameter
+                var atVenue = location.toLowerCase().indexOf(forVenue.toLowerCase()) > -1;
+                return (!preseason && atVenue);
+            }).each(function() {
+                var data = $(this);
+                var title = data.children('.event-listing-details').first()
+                                .children('.event-listing-title').first()
+                                .children().first().text().trim();
+                console.log(title);
+            });
+        }
+    });
+}
