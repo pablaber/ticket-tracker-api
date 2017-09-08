c22bbfa5c056334dceaaa8b4a7ea2aa9b1877f11
Patrick Bacon-Blaber <pablaber225@gmail.com>
2017-09-08 17:56:41 -0400

    add promise object

--- scrape.js
+++ scrape.js
@@ -13,38 +13,82 @@ function scrape() {
         var page = process.argv[2];
         var venue = process.argv[3];
         var SEAT_GEEK = "https://seatgeek.com/";
-        getTodaysPrices(SEAT_GEEK + page, venue);
+        getTodaysPrices(SEAT_GEEK + page, venue).then(function(todaysPrices) {
+            console.log(todaysPrices);
+        });
     }
 }
 
 function getTodaysPrices(forUrl, forVenue) {
-    request(forUrl, function(error, resopnse, html) {
-        if(!error) {
-            var $ = cheerio.load(html);
+    return new Promise(function(resolve, reject) {
+        var todaysPrices = {};
 
-            $('.page-event-listing').filter(function() {
-                var data = $(this);
-                var title = data.children('.event-listing-details').first()
-                                .children('.event-listing-title').first()
-                                .children().first().text().trim();
+        request(forUrl, function(error, resopnse, html) {
+            if(!error) {
+                var $ = cheerio.load(html);
 
-                var location = data.children('.event-listing-details').first()
-                                   .children('.event-listing-location').first()
-                                   .children('.event-listing-location-text').first()
-                                   .children('.event-listing-venue-link').first()
-                                   .children().first().text().trim();
+                var dates = [];
+                $('.page-event-listing').filter(function() {
+                    var data = $(this);
+                    var title = data.children('.event-listing-details').first()
+                                    .children('.event-listing-title').first()
+                                    .children().first().text().trim();
 
-                var preseason = title.toLowerCase().indexOf('preseason') > -1;
+                    var location = data.children('.event-listing-details').first()
+                                       .children('.event-listing-location').first()
+                                       .children('.event-listing-location-text').first()
+                                       .children('.event-listing-venue-link').first()
+                                       .children().first().text().trim();
 
-                var atVenue = location.toLowerCase().indexOf(forVenue.toLowerCase()) > -1;
-                return (!preseason && atVenue);
-            }).each(function() {
-                var data = $(this);
-                var title = data.children('.event-listing-details').first()
-                                .children('.event-listing-title').first()
-                                .children().first().text().trim();
-                console.log(title);
-            });
-        }
+                    var date = data.children('.event-listing-datetime').first()
+                                   .children('.event-listing-date').first().text().trim();
+
+                    var duplicate = dates.includes(date);
+
+                    if(!duplicate) {
+                        dates.push(date);
+                    }
+
+                    var preseason = title.toLowerCase().indexOf('preseason') > -1;
+
+                    var atVenue = location.toLowerCase().indexOf(forVenue.toLowerCase()) > -1;
+                    return (!preseason && atVenue && !duplicate);
+                }).each(function() {
+                    var data = $(this);
+                    var title = data.children('.event-listing-details').first()
+                                    .children('.event-listing-title').first()
+                                    .children().first().text().trim();
+
+                    var date = data.children('.event-listing-datetime').first()
+                                   .children('.event-listing-date').first().text().trim().replace(/ +(?= )/g,'');
+
+                    var time = data.children('.event-listing-datetime').first()
+                                   .children('.event-listing-time').first().text().trim().replace(/ +(?= )/g,'');
+
+                    var price = parseInt(data.children('.event-listing-button').first().text().trim().substring(6));
+
+                    var datetime = moment(date + " " + time, "MMM D ddd h:mm a");
+
+                    if(datetime.diff(moment()) < 0) {
+                        datetime.add(1, 'years');
+                    }
+
+                    var gameid = datetime.format("YYYYMMDD");
+
+                    todaysPrices[gameid] = {
+                        title: title,
+                        datetime: datetime,
+                        price: price
+                    };
+                });
+
+                resolve(todaysPrices);
+            }
+            else {
+                reject(-1);
+            }
+        });
+
     });
+
 }
